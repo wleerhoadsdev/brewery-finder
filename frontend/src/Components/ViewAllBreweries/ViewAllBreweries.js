b800187b0730294ff827db5b9c2d5bfad714a806
oscar <gallardo_oscar@outlook.com>
2022-11-10 17:54:38 -0600

    retireved props from data passed down to component, to display brewery_name

--- frontend/src/Components/ViewAllBreweries/ViewAllBreweries.js
+++ frontend/src/Components/ViewAllBreweries/ViewAllBreweries.js
@@ -6,10 +6,21 @@ export default function ViewAllBreweries(props) {
     //     <h3>Welcome back {props.user.username}!</h3> :
     //     '';
 
+    const role = props.user ? props.user.authorities[0].name : '';
+    const {brewery_name, phone_number, hours_of_operation, address} = props.data.breweryData;
+    console.log('role: ' + role);
+    console.log('breweryName: ' + brewery_name);
+    console.log('breweryPhone: ' + phone_number);
+    console.log('breweryHours: ' + hours_of_operation);
+    console.log('breweryAddress: ' + address);
+
     return (
-        <div className='home--left-panel'>
+        <div className='breweries'>
             {/* {userText} */}
             <h3> Below are our List of Breweries!</h3>
+            <div className="breweries--list">
+                <h1>{brewery_name}</h1>
+            </div>
             <Link to='/'>View All Breweries</Link>
         </div>
     )
