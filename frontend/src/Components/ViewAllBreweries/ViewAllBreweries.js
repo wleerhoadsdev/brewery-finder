f1300c8cf3736bc2ab3c684e0bd0d25a9a2ea42c
oscar <gallardo_oscar@outlook.com>
2022-11-10 20:01:15 -0600

    Implemented API call to get the list of breweries and display info on page

--- frontend/src/Components/ViewAllBreweries/ViewAllBreweries.js
+++ frontend/src/Components/ViewAllBreweries/ViewAllBreweries.js
@@ -1,27 +1,49 @@
+import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom'
+import { baseUrl } from '../../Shared/baseUrl';
 
 export default function ViewAllBreweries(props) {
 
-    // const userText = props.user ?
-    //     <h3>Welcome back {props.user.username}!</h3> :
-    //     '';
-
+    
     const role = props.user ? props.user.authorities[0].name : '';
-    const {brewery_name, phone_number, hours_of_operation, address} = props.data.breweryData;
-    console.log('role: ' + role);
-    console.log('breweryName: ' + brewery_name);
-    console.log('breweryPhone: ' + phone_number);
-    console.log('breweryHours: ' + hours_of_operation);
-    console.log('breweryAddress: ' + address);
+    const [breweriesData, setBreweriesData] = React.useState([]);
+
+    console.log(breweriesData);
+
+    React.useEffect(() => {
+        axios.get(baseUrl + '/brewery').then((response) => {
+            setBreweriesData(response.data);
+        })
+    }, []);
+
+    const breweryElements = breweriesData.map(brewery => {
+        return (
+            <tr>
+                <td>{brewery.name}</td>
+                <td>{brewery.address.city}</td>
+                <td>{brewery.address.state}</td>
+            </tr>
+        )  
+    })
 
     return (
         <div className='breweries'>
             {/* {userText} */}
             <h3> Below are our List of Breweries!</h3>
-            <div className="breweries--list">
-                <h1>{brewery_name}</h1>
-            </div>
-            <Link to='/'>View All Breweries</Link>
+            <Link to='/'>Go Back Home</Link>
+            <table>
+                <tr>
+                    <th>Brewery Name</th>
+                    <th>City</th>
+                    <th>State</th>
+                </tr>
+                {breweryElements}
+            </table>
+            <img
+                src='https://via.placeholder.com/600'
+                alt='placeholder'
+                className='ViewAllBreweries__image' />
         </div>
     )
 }
