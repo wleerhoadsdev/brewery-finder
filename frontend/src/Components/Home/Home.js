65ab143a80e20c911989838fe1f231cfd902a4d4
Lawrence Chen <lawrencegchen96@gmail.com>
2022-11-11 22:21:45 -0600

    Changed Home to conditionally render link to ViewAllUsers when logged in as admin and fleshed out ViewAllUsers page.

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -5,7 +5,21 @@ export default function Home(props) {
     const userText = props.user ?
         <h3 className='home__greeting'>Welcome back <span className='home__user'>{props.user.username}</span>!</h3> :
         '';
-
+    const role = props.user ? props.user.authorities[0].name : '';
+    if(role==="ROLE_ADMIN"){
+        return (
+            <div className='home__left-panel'>
+                {userText}
+                <h3>Discover your new favorite beer</h3>
+                <Link to='/ViewAllBreweries'>View All Breweries</Link>
+                <Link to='/ViewAllUsers'>View All Users</Link>
+                <img
+                    src='https://via.placeholder.com/600'
+                    alt='placeholder'
+                    className='home__image' />
+            </div>
+        )
+    }
     return (
         <div className='home__left-panel'>
             {userText}
--- frontend/src/Components/ViewAllUsers/ViewAllUsers.js
+++ frontend/src/Components/ViewAllUsers/ViewAllUsers.js
@@ -1,15 +1,59 @@
+import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom'
+import { baseUrl } from '../../Shared/baseUrl';
 
 export default function ViewAllUsers(props) {
 
     const userText = props.user ?
         <h3>Welcome back {props.user.username}!</h3> :
         '';
+    const role = props.user ? props.user.authorities[0].name:'';
+    const [usersData,setUsersData]=React.useState([]);
+
+    React.useEffect(()=>{
+        axios.get(baseUrl+'/usersbreweries').then((response)=>{
+            setUsersData(response.data);
+        })
+    })
+
+    const usersElements=usersData.map(userBreweryListItem=>{
+        if(role=='ROLE_ADMIN'&&userBreweryListItem.breweryName!=null){
+            return(
+                <tr key={userBreweryListItem.userId}>
+                    <td>{userBreweryListItem.username}</td>
+                    <td>{userBreweryListItem.name}</td>
+                    <td>{userBreweryListItem.breweryName}</td>
+                </tr>
+            )
+        }
+        else if(role=='ROLE_ADMIN'){
+            return(
+                <tr key={userBreweryListItem.userId}>
+                    <td>{userBreweryListItem.username}</td>
+                    <td>{userBreweryListItem.name}</td>
+                    <td><Link to='/AddBrewery'><button>Create Brewery</button></Link></td>
+                </tr>
+            )
+        }
+        else{
+            return(
+                <tr key="Error Message">You are not authorized to view this page.</tr>
+            )
+        }
+    })
 
     return (
         <div className='home--left-panel'>
-            {userText}
             <h3> Page to View All Users</h3>
+            <table>
+                <tr>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>Brewery</th>
+                </tr>
+                {usersElements}
+            </table>
             <Link to='/'>View All Breweries</Link>
             <img src='https://via.placeholder.com/600' alt='placeholder' />
         </div>
