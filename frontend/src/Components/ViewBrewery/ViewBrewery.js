dce173d0ba47ad3aad413428506424e3f5263d1c
oscar <gallardo_oscar@outlook.com>
2022-11-11 22:42:23 -0600

    Retrieved individual brewery data from the api based on id and displayed the info on the page

--- frontend/src/Components/ViewBrewery/ViewBrewery.js
+++ frontend/src/Components/ViewBrewery/ViewBrewery.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { baseUrl } from '../../Shared/baseUrl';
 
 export default function ViewBrewery(props) { 
 
-    const [breweryData, setBreweryData] = React.useState();
+    const location = useLocation();
+    const [breweryData, setBreweryData] = React.useState({});
+    const [address, setAddress] = React.useState("");
+    const {id} = location.state;
 
     React.useEffect(() => {
-        console.log(props);
-        
-        // axios.get(baseUrl + `/brewery/${id}`).then((response) => {
-        //     setBreweryData(response.data);
-        // });
+        axios.get(baseUrl + `/brewery/${id}`).then((response) => {
+            const {street, city, state, zipCode, country} = response.data.address;
+            setAddress(`${street} ${city} ${state} ${zipCode} ${country}`);
+            setBreweryData(response.data);
+         });
     }, []);
-    
-
-    // const breweryElement = breweryData.map((brewery) => {
-    //     return (
-    //         <div>
-    //             <h1>{brewery.name}</h1>
-    //         </div>
-    //     );
-    // });
 
     return (
         <div className='home--left-panel'>
             <h3>Brewery Info</h3>
             <Link to='/ViewAllBreweries'>Go Back to Listing</Link>
-            {/* {breweryElement} */}
+            <p>{breweryData.name}</p>
+            <p>{breweryData.emailAddress}</p>
+            <p>{breweryData.history}</p>
+            <p>{breweryData.hoursOfOperation}</p>
+            <p>{address}</p>
+            <p>{breweryData.phoneNumber}</p>
         </div>
     )
 }
