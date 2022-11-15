85dd7373bf380dcc89c15a82ae7d9de0b675a79f
oscar <gallardo_oscar@outlook.com>
2022-11-15 14:16:34 -0600

    Updated the submit handle to make the api call to update the database

--- frontend/src/Components/EditBrewery/EditBrewery.js
+++ frontend/src/Components/EditBrewery/EditBrewery.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
+import { baseUrl} from '../../Shared/baseUrl';
 
 export default function EditBrewery(props) {
 
@@ -8,7 +10,7 @@ export default function EditBrewery(props) {
     const breweryData = location.state.breweryData;
     const [formData, setFormData] = React.useState({
         name: breweryData.name || "",
-        email: breweryData.emailAddress || "",
+        emailAddress: breweryData.emailAddress || "",
         history: breweryData.history || "",
         hoursOfOperation: breweryData.hoursOfOperation || "",
         address: {
@@ -18,7 +20,9 @@ export default function EditBrewery(props) {
             zipCode: breweryData.address.zipCode || "",
             country: breweryData.address.country || ""
         },
-        phoneNumber: breweryData.phoneNumber || ""
+        phoneNumber: breweryData.phoneNumber || "",
+        homePageUrl: breweryData.homePageUrl || "",
+        imageUrl: breweryData.imageUrl || ""
     });
     const {street, city, state, zipCode, country} = formData.address;
 
@@ -39,7 +43,23 @@ export default function EditBrewery(props) {
 
     function handleSubmit(event) {
         event.preventDefault();
-        console.log(formData);
+
+        const data = {
+            ...breweryData,
+            ...formData,
+            isActive: true,
+            isApproved: true,
+        }
+        console.log(data);
+        axios.put(baseUrl + `/brewery/${breweryId}`, data)
+            .then(response => {
+                alert("Brewery infomation was updated!");
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error.respone.data.error + ': ' + error.response.data.message);
+                alert("Brewery information was not updated!");
+            });
     }
 
     return (
@@ -47,7 +67,7 @@ export default function EditBrewery(props) {
             <h3 className='edit_brewery_heading'> Edit Existing Brewery Information</h3>
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Brewery Name" name="name" value={formData.name} onChange={handleChange} />
-                <input type="email" placeholder="Brewery Email" name="email" value={formData.email} onChange={handleChange} />
+                <input type="email" placeholder="Brewery Email" name="emailAddress" value={formData.emailAddress} onChange={handleChange} />
                 <input type="text" placeholder="Brewery History" name="history" value={formData.history} onChange={handleChange} />
                 <input type="text" placeholder="Brewery Hours of Operation" name="hoursOfOperation" value={formData.hoursOfOperation} onChange={handleChange} />
                 <input type="text" placeholder="Brewery Address: Street Address" name="street" value={street} onChange={handleChange} />
@@ -56,6 +76,8 @@ export default function EditBrewery(props) {
                 <input type="text" placeholder="Brewery Address: ZipCode" name="zipCode" value={zipCode} onChange={handleChange} />
                 <input type="text" placeholder="Brewery Address: Country" name="country" value={country} onChange={handleChange} />
                 <input type="text" placeholder="Brewery Phone Number" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+                <input type="text" placeholder="Brewery Home Page Url" name="homePageUrl" value={formData.homePageUrl} onChange={handleChange} />
+                <input type="text" placeholder="Brewery Image Url" name = "imageUrl" value={formData.imageUrl} onChange={handleChange} />
                 <button>Submit</button>
             </form>
             <br />
