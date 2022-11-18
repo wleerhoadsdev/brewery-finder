377c87255f28341dc6998f1becffc36d33854e84
PatrickETucker <petucker@outlook.com>
2022-11-17 18:06:35 -0800

    adds avg rating to ViewBeerList

--- frontend/src/Components/ViewBeerList/ViewBeerList.js
+++ frontend/src/Components/ViewBeerList/ViewBeerList.js
@@ -1,32 +1,51 @@
 import React from 'react'
 import axios from 'axios';
-import { Link,useLocation } from 'react-router-dom'
-import { baseUrl } from '../../Shared/baseUrl'; 
+import { Link, useLocation } from 'react-router-dom'
+import { baseUrl } from '../../Shared/baseUrl';
 
 export default function ViewBeerList(props) {
-    const location=useLocation();
-    const role = props.user ? props.user.authorities[0].name:'';
+    const location = useLocation();
+    const role = props.user ? props.user.authorities[0].name : '';
     const [beersData, setBeersData] = React.useState([]);
     const [breweryData, setBreweryData] = React.useState({});
-    const {isMyBrewery, breweryId}= location.state;
-    
+    const [beerRatings, SetBeerRatings] = React.useState();
+    const { isMyBrewery, breweryId } = location.state;
+
     React.useEffect(() => {
         axios.get(baseUrl + `/brewery/${breweryId}/beer`).then((response) => {
             setBeersData(response.data)
-         });
-         axios.get(baseUrl + `/brewery/${breweryId}`).then((response) => {
+        });
+        axios.get(baseUrl + `/brewery/${breweryId}`).then((response) => {
             setBreweryData(response.data);
         });
     }, [])
 
-    function handleActiveChange(e, beerId){
-        
+    React.useEffect(() => {
+        let newBeerRating = {}
+        axios.get(baseUrl + `/brewery/${breweryId}/beer/avgrating`)
+            .then((response) => {
+                response.data.forEach(rating => {
+                    const beerId = rating.beerId;
+                    const averageRating = rating.averageRating;
+                    newBeerRating = {
+                        ...newBeerRating,
+                        [beerId]: averageRating
+                    }
+                });
+                SetBeerRatings(newBeerRating);
+            });
+    }, []);
+
+    console.log(beerRatings);
+
+    function handleActiveChange(e, beerId) {
+
         const data = beersData.filter((beer) => beer.beerId === beerId)[0];
         const isActive = data.isActive;
         data.isActive = !isActive;
 
         axios.put(`${baseUrl}/brewery/${breweryId}/beer/${beerId}`, data)
-            .then(response =>{
+            .then(response => {
                 alert("Beer is now " + (isActive ? "Inactive" : "Active"));
             })
             .catch((error) => {
@@ -48,32 +67,33 @@ export default function ViewBeerList(props) {
     }
 
     const beerListElements = beersData.map((beer) => {
-        const data={
-            beerId:beer.beerId,
-            breweryId:beer.breweryId
+        const data = {
+            beerId: beer.beerId,
+            breweryId: beer.breweryId
         }
-        if(isMyBrewery||beer.isActive){
+        if (isMyBrewery || beer.isActive) {
+            const beerRating = beerRatings[data.beerId];
             return (
                 <div>
                     <tr key={beer.id}>
                         <td>
-                        <Link to={{ pathname: `/ViewBeerInformation/${beer.beerId}`, state: {data: data, isMyBrewery: isMyBrewery} }}>
-                                    {beer.name}
+                            <Link to={{ pathname: `/ViewBeerInformation/${beer.beerId}`, state: { data: data, isMyBrewery: isMyBrewery } }}>
+                                {beer.name}
                             </Link>
-
                         </td>
-                        {/* TODO: update typeId once we figure that out*/}
                         <td>{beer.typeId}</td>
                         <td>{beer.description}</td>
                         <td>{beer.abv}</td>
+                        <td>{beerRating}</td>
+                        {isMyBrewery && (role === "ROLE_BREWER") ? <td> <button onClick={(e) => { handleActiveChange(e, beer.beerId) }}>Toggle beer to {beer.isActive ? "Inactive" : "Active"}</button> </td> : ""}
                     </tr>
-                    {isMyBrewery && (role === "ROLE_BREWER") ? <button onClick={(e) => {handleActiveChange(e, beer.beerId)}}>Toggle beer to {beer.isActive ? "Inactive" : "Active"}</button> : ""}
+
                 </div>
             )
         }
-        else{return <div></div>}
+        else { return <div></div> }
     })
-    
+
 
     return (
         <main>
@@ -87,13 +107,15 @@ export default function ViewBeerList(props) {
                             <th>Type</th>
                             <th>Description</th>
                             <th>ABV</th>
+                            <th>AVG Rating</th>
+                            {isMyBrewery && <th>Active Toggle</th>}
                         </tr>
                     </thead>
                     <tbody>
                         {beerListElements}
                     </tbody>
                 </table>
-                {isMyBrewery ? <Link to={{pathname: `/AddBeer/${breweryId}`, state: { breweryData: breweryData }}}>Add Beer</Link> : ""}
+                {isMyBrewery ? <Link to={{ pathname: `/AddBeer/${breweryId}`, state: { breweryData: breweryData } }}>Add Beer</Link> : ""}
             </div>
             <div className='main__image-panel'>
                 <img src='https://via.placeholder.com/600' alt='placeholder' />
