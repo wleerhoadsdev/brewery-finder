996ec3f4d4ef9e9da4b2751d31e2516369d87cb1
PatrickETucker <petucker@outlook.com>
2022-11-20 22:51:31 -0800

    updated Home Link so it doesn't appear on Home page

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css';
 
 export default function Navbar(props) {
-  const nav = props.user ?
-    <nav>
-      <Link to='/' className='nav_app_title'>Beer Lovers</Link>
-      <Link to='/'>HOME</Link>
-      <Link to='/breweries'>VIEW ALL BREWERIES</Link>
-      <Link to='/' onClick={props.handleLogout}>LOG OUT</Link>
-    </nav>
-    :
-    <nav>
-      <Link to='/' className='nav_app_title'>Beer Lovers</Link>
-      <Link to='/'>HOME</Link>
-      <Link to='/login'>SIGN IN</Link>
-      <Link to='/register'>CREATE ACCOUNT</Link>
-    </nav>
+    const currentLocation = useLocation();
+    const isHomePage = currentLocation.pathname === '/';
 
+    const nav = props.user ? (
+        <nav>
+            <Link
+                to='/'
+                className='nav_app_title'
+            >
+                Beer Lovers
+            </Link>
+            {!isHomePage && <Link to='/'>HOME</Link>}
+            <Link to='/breweries'>VIEW ALL BREWERIES</Link>
+            <Link
+                to='/'
+                onClick={props.handleLogout}
+            >
+                LOG OUT
+            </Link>
+        </nav>
+    ) : (
+        <nav>
+            <Link
+                to='/'
+                className='nav_app_title'
+            >
+                Beer Lovers
+            </Link>
+            {!isHomePage && <Link to='/'>HOME</Link>}
+            <Link to='/login'>SIGN IN</Link>
+            <Link to='/register'>CREATE ACCOUNT</Link>
+        </nav>
+    );
 
-  return (
-    <header>
-      {nav}
-    </header>
-  )
-
+    return <header>{nav}</header>;
 }
